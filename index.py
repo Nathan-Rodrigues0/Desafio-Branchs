9ff6ccef736b17c3b177cdf5ce8a78d7114cd410
Nathan Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2026-04-18 02:18:58 -0300

    segunda parte do código na master2 completo!

--- index.py
+++ index.py
@@ -1,4 +1,21 @@
+print("Criando a segunda parte do código")
 
+i = int(input("Digte um número de repetição: "))
+num = 2
+
+while True:
+    print("Código feito")
+    print("Você conseguiu")
+    print("Parabéns")
+    if num <= i:
+        num += 1
+        continue
+    elif num > i :
+        print("Finalizado")
+        break
+    else:
+        print("Inacessível")
+        break
 
 
 
